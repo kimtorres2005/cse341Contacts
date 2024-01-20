410097aaf7df707ba37a4f912171b179a190b24e
kimtorres2005 <kimtorres2005@gmail.com>
2024-01-20 14:54:42 -0600

    Update swagger version in package.json and remove generateapikey.js

--- generateapikey.js
+++
@@ -1,8 +0,0 @@
-const crypto = require('crypto');
-
-function generateApiKey() {
-  // Generate a random 32-character hexadecimal string
-  return crypto.randomBytes(16).toString('hex');
-}
-
-console.log('Generated API Key:', generateApiKey());
